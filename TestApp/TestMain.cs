5d55b61b38f762305192dbfbe31a2c768c6c9d30
Vincent-Decrauzat <vincent.decrauzat@smartliberty.ch>
2015-02-18 14:08:11 +0100

    Added TestMain that was forgotten

---
+++ TestApp/TestMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class TestMain
+    {
+        public void DoTheJob()
+        {
+            Console.WriteLine("Doing my job...");
+            Console.WriteLine("Some more work...");
+        }
+    }
+}
